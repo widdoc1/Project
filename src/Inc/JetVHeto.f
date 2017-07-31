4ebe51efb4f9c25e12a78c189a61a5d41393388b
lcarpino <luke.c.arpino@gmail.com>
2017-07-31 17:08:42 +0100

    added dynamic scales for resummation and container for resummation parameters

---
+++ src/Inc/JetVHeto.f
@@ -0,0 +1,17 @@
+!     scales used in the JetVHeto resummation
+      logical :: resm          ! whether resummation is to be performed
+
+! scale variation of ptjet
+      real(dp) :: Q_scale       ! resummation scale Q
+      real(dp) :: Q_scalestart
+! small R resummation
+      real(dp) :: R_scale       ! resummation scale R
+      real(dp) :: R_scalestart
+
+! container for resummation ingredients
+      type(process_and_parameters) :: resm_opts
+
+      common/resminputs/Q_scalestart,R_scalestart
+      common/resmscale/Q_scale,R_scale
+      common/resmparams/resm_opts
+!$omp threadprivate(/resmscale)
